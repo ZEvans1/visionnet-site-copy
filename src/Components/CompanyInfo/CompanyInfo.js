8285386352c220b16815cd0a504077785fe95828
zach <zte.zachary@gmail.com>
2018-09-11 14:46:57 -0700

    Center and align cols and rows.

--- src/Components/CompanyInfo/CompanyInfo.js
+++ src/Components/CompanyInfo/CompanyInfo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, Image } from 'react-bootstrap';
+import { Grid, Row, Col, Image, Button } from 'react-bootstrap';
 import './CompanyInfo.css';
 
 class CompanyInfo extends Component {
@@ -14,33 +14,38 @@ class CompanyInfo extends Component {
                 </div>
                 <div className="company-card-grid-div">
                     <Grid>
-                        <Row>
+                        <Row style={{display: 'flex', justifyContent: 'space-evenly', marginTop: '20px', marginBottom: '50px'}}>
                             <Col lg={6}>
                                 <Image className="card-image" alt="servers" src="http://vision.net/images/page-title1.jpg"/>
                                 <p><strong>Network</strong></p>
                                 <p className="grey-description">Montana's Largest State-wide Network</p>
+                                <Button>Learn More</Button>
                             </Col>
                             <Col lg={6}>
                                 <Image className="card-image" alt="server cage" src="http://vision.net/images/page-title5.jpg"/>
                                 <p><strong>Enterprise Cloud Infrastructure</strong></p>
                                 <p className="grey-description">Vision Net can protect, store, and transport your data.</p>
+                                <Button>Learn More</Button>
                             </Col>
                         </Row>
-                        <Row>
+                        <Row style={{display: 'flex', justifyContent: 'space-evenly', marginTop: '20px', marginBottom: '50px'}}>
                             <Col lg={4}>
                                 <Image className="card-image" alt="man at computers" src="http://vision.net/images/managed_services.jpg"/>
                                 <p><strong>Business Services</strong></p>
                                 <p className="grey-description">Vision Net can effectively monitor and manage your internet, fiber & IT infrastructure.</p>
+                                <Button>Learn More</Button>
                             </Col>
                             <Col lg={4}>
                                 <Image className="card-image" alt="man at computers" src="http://vision.net/images/support_services.jpg"/>
                                 <p><strong>Support Services</strong></p>
                                 <p className="grey-description">Our support center delivers full range technical support services designed to enhance your productivity & image.</p>
+                                <Button>Learn More</Button>
                             </Col>
                             <Col lg={4}>
                                 <Image className="card-image" alt="Vision Net logo" src="http://vision.net/images/generic2.jpg"/>
                                 <p><strong>About Vision Net</strong></p>
                                 <p className="grey-description">Read our Mission, Values and Company Culture Statement, as well as keep up on current news and announcements.</p>
+                                <Button>Learn More</Button>
                             </Col>
                         </Row>
                     </Grid>
