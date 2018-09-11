c59edfcddf29a0870154e3d19634b33dd7d84799
zach <zte.zachary@gmail.com>
2018-09-11 14:26:21 -0700

    Add images and text description for rows and columns.

--- src/Components/CompanyInfo/CompanyInfo.js
+++ src/Components/CompanyInfo/CompanyInfo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Image } from 'react-bootstrap';
 import './CompanyInfo.css';
 
 class CompanyInfo extends Component {
@@ -16,21 +16,31 @@ class CompanyInfo extends Component {
                     <Grid>
                         <Row>
                             <Col lg={6}>
-                                <p>Network</p>
+                                <Image className="card-image" alt="servers" src="http://vision.net/images/page-title1.jpg"/>
+                                <p><strong>Network</strong></p>
+                                <p className="grey-description">Montana's Largest State-wide Network</p>
                             </Col>
                             <Col lg={6}>
-                                <p>Enterprise Cloud Infrastructure</p>
+                                <Image className="card-image" alt="server cage" src="http://vision.net/images/page-title5.jpg"/>
+                                <p><strong>Enterprise Cloud Infrastructure</strong></p>
+                                <p className="grey-description">Vision Net can protect, store, and transport your data.</p>
                             </Col>
                         </Row>
                         <Row>
                             <Col lg={4}>
-                                <p>Business Services</p>
+                                <Image className="card-image" alt="man at computers" src="http://vision.net/images/managed_services.jpg"/>
+                                <p><strong>Business Services</strong></p>
+                                <p className="grey-description">Vision Net can effectively monitor and manage your internet, fiber & IT infrastructure.</p>
                             </Col>
                             <Col lg={4}>
-                                <p>Support Services</p>
+                                <Image className="card-image" alt="man at computers" src="http://vision.net/images/support_services.jpg"/>
+                                <p><strong>Support Services</strong></p>
+                                <p className="grey-description">Our support center delivers full range technical support services designed to enhance your productivity & image.</p>
                             </Col>
                             <Col lg={4}>
-                                <p>About Vision Net</p>
+                                <Image className="card-image" alt="Vision Net logo" src="http://vision.net/images/generic2.jpg"/>
+                                <p><strong>About Vision Net</strong></p>
+                                <p className="grey-description">Read our Mission, Values and Company Culture Statement, as well as keep up on current news and announcements.</p>
                             </Col>
                         </Row>
                     </Grid>
